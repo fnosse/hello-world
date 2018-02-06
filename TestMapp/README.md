511f50ff26419a5850731f6ce5e16422fa3dfbe9
nils <nilsru@kth.se>
2018-02-06 15:08:04 +0100

    Big changes

--- TestMapp/README.md
+++
@@ -1,3 +0,0 @@
-Hej,
-
-En till README
---
+++ src/test/java/TestHelloWorld.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class TestHelloWorld {
+
+  @Test
+  public void testAssert() {
+
+    assertTrue(true);
+  }
+}
